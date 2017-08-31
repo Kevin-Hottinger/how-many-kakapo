a69ddbb45c4cd1432b4ee9fab4819410cc0d77f8
Kevin Hottinger <kevinhottinger@gmail.com>
2017-09-01 01:02:54 +1200

    [JS] Include stand-alone page components

---
+++ src/js/components/NotFound.jsx
@@ -0,0 +1,13 @@
+// Include React
+import React from '../../../setup/node_modules/react';
+// Import KakapoHeader component
+import Header from '../components/Header.jsx';
+
+// Display image
+const NotFound = () => (
+	<div className="application">
+		<Header title="Page not found"/>
+	</div>
+);
+
+export default NotFound;
