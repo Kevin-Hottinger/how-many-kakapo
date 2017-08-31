cd2b485d1020d90c05cc853425f30cf2f542e1ad
Kevin Hottinger <kevinhottinger@gmail.com>
2017-09-01 01:06:49 +1200

    [JS] Navigation
    Hook up navigation to react router4

--- src/js/components/Navigation.jsx
+++ src/js/components/Navigation.jsx
@@ -1,38 +1,39 @@
 // Include React
 import React from '../../../setup/node_modules/react';
+// Include router link
+import { NavLink } from '../../../setup/node_modules/react-router-dom';
 
 // Display image
-const Navigation = () => {
-	return(
-		<nav>
-			<ul>
-				<li>
-					<a href="" title="Home">
-						<i className="fa fa-home fa-fw"></i>
-						<span>&nbsp; Home</span>
-					</a>
-				</li>
-				<li>
-					<a href="" title="News">
-						<i className="fa fa-newspaper-o fa-fw"></i>
-						<span>&nbsp; News</span>
-					</a>
-				</li>
-				<li>
-					<a href="" title="Graph">
-						<i className="fa fa-line-chart fa-fw"></i>
-						<span>&nbsp; Graph</span>
-					</a>
-				</li>
-				<li>
-					<a href="" title="Share">
-						<i className="fa fa-share-alt fa-fw"></i>
-						<span>&nbsp; Share</span>
-					</a>
-				</li>
-			</ul>
-		</nav>
-	);
-};
+const Navigation = () => (
+	<nav>
+		<ul>
+			<li>
+				<NavLink exact to="/" title="Home">
+					<i className="fa fa-home fa-fw"></i>
+					<span>&nbsp; Home</span>
+				</NavLink>
+			</li>
+			<li>
+				<NavLink to="/news" title="News">
+					<i className="fa fa-newspaper-o fa-fw"></i>
+					<span>&nbsp; News</span>
+				</NavLink>
+			</li>
+			<li>
+				<NavLink to="/graph" title="Graph">
+					<i className="fa fa-line-chart fa-fw"></i>
+					<span>&nbsp; Graph</span>
+				</NavLink>
+			</li>
+			<li>
+				<NavLink to="/share" title="Share">
+					<i className="fa fa-share-alt fa-fw"></i>
+					<span>&nbsp; Share</span>
+				</NavLink>
+			</li>
+		</ul>
+	</nav>
+);
+
 
 export default Navigation;
