69cb31a798ba31802d38c37f970ad635f19dd8f4
Kevin Hottinger <kevinhottinger@gmail.com>
2017-09-01 01:09:10 +1200

    [JS] Incorperate multipage support

---
+++ src/js/components/Header.jsx
@@ -0,0 +1,11 @@
+// Include React
+import React from '../../../setup/node_modules/react';
+
+// Display Header
+const Header = props => (
+	<header>
+		<h1>{props.title}</h1>
+	</header>
+);
+
+export default Header;
